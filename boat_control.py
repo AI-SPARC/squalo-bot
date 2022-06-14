313949dd50986f41c54e4a6c6e712f997fec8886
Hyago <hyago-brito@hotmail.com>
2022-06-14 15:13:02 -0300

    Update

--- boat_control.py
+++ boat_control.py
@@ -7,6 +7,7 @@ from zmqRemoteApi import RemoteAPIClient
 
 Ts = 0.05
 
+
 def quat2euler(h):
     roll = np.arctan2(2*(h[0]*h[1] + h[2]*h[3]), 1 - 2*(h[1]**2 + h[2]**2))
     pitch = np.arcsin(2*(h[0]*h[2] - h[3]*h[1]))
@@ -83,6 +84,17 @@ while abs(linear) > 0.3:
     time.sleep(Ts)
 
     #Recollect information
+    #Target
+    boat_target = sim.getObject('/Boat_Target')
+
+    position = sim.getObjectPosition(boat_target, -1)
+    orientation = sim.getObjectQuaternion(boat_target, -1)
+    quat = np.array([orientation[3], orientation[0], orientation[1], orientation[2]]) # Remember: getObjectQuaternion has real part as last element
+    (roll, pitch, yaw) = quat2euler(quat)
+
+    X_d = np.array([[position[0]], [position[1]], [position[2]], [roll], [pitch], [yaw]])
+
+    #Boat
     boat_position = sim.getObjectPosition(boat, -1)
     boat_orientation = sim.getObjectQuaternion(boat, -1)
     quat = np.array([boat_orientation[3], boat_orientation[0], boat_orientation[1], boat_orientation[2]]) # Remember: getObjectQuaternion has real part as last element
