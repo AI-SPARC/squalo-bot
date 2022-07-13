a4755c7fd5ad65340a8537cbd817d6c276e0080c
Hyago <hyago-brito@hotmail.com>
2022-07-12 21:23:32 -0300

    update

--- boat_control.py
+++ boat_control.py
@@ -59,16 +59,15 @@ sim = client.getObject('sim')
 
 sim.startSimulation()
 
+boat = sim.getObject('/Boat')
 boat_target = sim.getObject('/Boat_Target')
 
-position = sim.getObjectPosition(boat_target, -1)
-orientation = sim.getObjectQuaternion(boat_target, -1)
+position = sim.getObjectPosition(boat_target, boat)
+orientation = sim.getObjectQuaternion(boat_target, boat)
 quat = np.array([orientation[3], orientation[0], orientation[1], orientation[2]]) # Remember: getObjectQuaternion has real part as last element
-(roll, pitch, yaw) = quat2euler(quat)
+(roll, pitch, target_yaw) = quat2euler(quat)
 
-X_d = np.array([[position[0]], [position[1]], [position[2]], [roll], [pitch], [yaw]])
-
-boat = sim.getObject('/Boat')
+X_d = np.array([[position[0]], [position[1]], [position[2]], [roll], [pitch], [target_yaw]])
 
 boat_position = sim.getObjectPosition(boat, -1)
 boat_orientation = sim.getObjectQuaternion(boat, -1)
@@ -81,38 +80,33 @@ X_m = np.array([[boat_position[0]], [boat_position[1]], [boat_position[2]], [rol
 boat_data_x = []
 boat_data_y = []
 target_position = [position[0], position[1]]
-initial_position = [boat_position[0], boat_position[1]]
+initial_position = [0, 0]
 boat_data_x.append(boat_position[0])
 boat_data_y.append(boat_position[1])
 
-setpoint = np.ravel([[position[0], position[1], 0, 0] for i in range(0, N+1)])
+setpoint = np.ravel([[np.sqrt(position[0]**2 + position[1]**2), np.arctan2(position[1], position[0]), 0, 0] for i in range(0, N+1)])
 mpc = MPC(mass, np.array(initial_position), v_min, v_max, N, N_c, Ts)
 result = mpc.getNewForce(setpoint)
 force = result[1]
 print(force)
 
-error = get_error(X_d, X_m)
-v_x, v_y, yaw = extract_error_information(error, X_m)
-angular = extract_angular(force[0], force[1], yaw)
-linear = extract_linear(force[0], force[1])
-#angular = extract_angular(v_x, v_y, yaw)
-#linear = extract_linear(v_x, v_y)
+Fr = 0
+Fl = 0
 
-
-while abs(linear) > 0.3:
-    print('Linear: ' + str(linear))
-    print('Angular: ' + str(angular))
-    sim.addForce(boat, [1,0,0] , [linear, 0, 0])
-    sim.addForceAndTorque(boat, [0, 0, 0] , [0, 0, angular])
+while True:
+    print('Final Fr: ' + str(Fr))
+    print('Final Fl: ' + str(Fl))
+    sim.addForce(boat, [-0.3, -0.2, 0] , [Fr, 0, 0]) # Right Motor
+    sim.addForce(boat, [-0.3, 0.2, 0] , [Fl, 0, 0]) # Left Motor
+    #sim.addForceAndTorque(boat, [0, 0, 0] , [0, 0, angular])
 
     time.sleep(Ts)
 
     #Recollect information
     #Target
-    boat_target = sim.getObject('/Boat_Target')
 
-    position = sim.getObjectPosition(boat_target, -1)
-    orientation = sim.getObjectQuaternion(boat_target, -1)
+    position = sim.getObjectPosition(boat_target, boat)
+    orientation = sim.getObjectQuaternion(boat_target, boat)
     quat = np.array([orientation[3], orientation[0], orientation[1], orientation[2]]) # Remember: getObjectQuaternion has real part as last element
     (roll, pitch, yaw) = quat2euler(quat)
 
@@ -125,6 +119,8 @@ while abs(linear) > 0.3:
     (roll, pitch, yaw) = quat2euler(quat)
     boat_velocity = sim.getObjectVelocity(boat, -1)
     v_x, v_y = extract_cartesian(boat_velocity)
+    v_linear = boat_velocity[0][0]
+    v_angular = boat_velocity[-1][-1]
 
 
     X_m = np.array([[boat_position[0]], [boat_position[1]], [boat_position[2]], [roll], [pitch], [yaw]])
@@ -132,15 +128,14 @@ while abs(linear) > 0.3:
     boat_data_y.append(boat_position[1])
 
     #Collect data
-    mpc.x_0 = np.array([boat_position[0], boat_position[1], v_x, v_y])
+    mpc.x_0 = np.array([0, 0, v_linear, v_angular])
+    setpoint = np.ravel([[np.sqrt(position[0]**2 + position[1]**2), np.arctan2(position[1], position[0]), 0, 0] for i in range(0, N+1)])
     result = mpc.getNewForce(setpoint)
     force = result[1]
     print(force)
 
-    error = get_error(X_d, X_m)
-    v_x, v_y, yaw = extract_error_information(error, X_m)
-    angular = extract_angular(force[0], force[1], yaw)
-    linear = extract_linear(force[0], force[1])
+    Fr = force[0]
+    Fl = force[1]
 
 print('Arrived')
 
