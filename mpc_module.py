57e56a423ef5a840a3a62f9c3443bb8250475992
Hyago <hyago-brito@hotmail.com>
2022-07-19 16:49:14 -0300

    mpc_module added

--- mpc_module.py
+++ mpc_module.py
@@ -15,43 +15,33 @@ timevarying_parameter 	_tvp 	Optional
 '''
 
 #Model Variables
-phi_1 = model.set_variable(var_type='_x', var_name='phi_1', shape=(1,1))
-phi_2 = model.set_variable(var_type='_x', var_name='phi_2', shape=(1,1))
-phi_3 = model.set_variable(var_type='_x', var_name='phi_3', shape=(1,1))
-# Variables can also be vectors:
-dphi = model.set_variable(var_type='_x', var_name='dphi', shape=(3,1))
+r = model.set_variable(var_type='_x', var_name='r', shape=(1,1))
+theta = model.set_variable(var_type='_x', var_name='theta', shape=(1,1))
+v = model.set_variable(var_type='_x', var_name='v', shape=(1,1))
+omega = model.set_variable(var_type='_x', var_name='omega', shape=(1,1))
+
 # Two states for the desired (set) motor position:
-phi_m_1_set = model.set_variable(var_type='_u', var_name='phi_m_1_set')
-phi_m_2_set = model.set_variable(var_type='_u', var_name='phi_m_2_set')
-# Two additional states for the true motor position:
-phi_1_m = model.set_variable(var_type='_x', var_name='phi_1_m', shape=(1,1))
-phi_2_m = model.set_variable(var_type='_x', var_name='phi_2_m', shape=(1,1))
+fr = model.set_variable(var_type='_u', var_name='fr')
+fl = model.set_variable(var_type='_u', var_name='fl')
 
 #Model Parameters
 # As shown in the table above, we can use Long names or short names for the variable type.
-Theta_1 = model.set_variable('parameter', 'Theta_1')
-Theta_2 = model.set_variable('parameter', 'Theta_2')
-Theta_3 = model.set_variable('parameter', 'Theta_3')
-
-c = np.array([2.697,  2.66,  3.05, 2.86])*1e-3
-d = np.array([6.78,  8.01,  8.82])*1e-5
+J = model.set_variable('parameter', 'J')
 
 #Right-hand-side equation
-model.set_rhs('phi_1', dphi[0])
-model.set_rhs('phi_2', dphi[1])
-model.set_rhs('phi_3', dphi[2])
 
-dphi_next = vertcat(
-    -c[0]/Theta_1*(phi_1-phi_1_m)-c[1]/Theta_1*(phi_1-phi_2)-d[0]/Theta_1*dphi[0],
-    -c[1]/Theta_2*(phi_2-phi_1)-c[2]/Theta_2*(phi_2-phi_3)-d[1]/Theta_2*dphi[1],
-    -c[2]/Theta_3*(phi_3-phi_2)-c[3]/Theta_3*(phi_3-phi_2_m)-d[2]/Theta_3*dphi[2],
-)
+d = 0.3
+l = 0.2
+m = 80
 
-model.set_rhs('dphi', dphi_next)
+v_dot = d*omega**2 + (1/m)*(fr+fl)
+omega_dot = (l/(m*d**2 + J))*(fr-fl) - (m*d*v*omega)/(m*d**2 + J)
 
-tau = 1e-2
-model.set_rhs('phi_1_m', 1/tau*(phi_m_1_set - phi_1_m))
-model.set_rhs('phi_2_m', 1/tau*(phi_m_2_set - phi_2_m))
+
+model.set_rhs('r', v)
+model.set_rhs('theta', omega)
+model.set_rhs('v', v_dot)
+model.set_rhs('omega', omega_dot)
 
 model.setup()
 
@@ -61,55 +51,41 @@ mpc = do_mpc.controller.MPC(model)
 #Optimizer parameters
 setup_mpc = {
     'n_horizon': 20,
-    't_step': 0.1,
+    't_step': 0.05,
     'n_robust': 1,
     'store_full_solution': True,
 }
 mpc.set_param(**setup_mpc)
 
 #Objective function
-mterm = phi_1**2 + phi_2**2 + phi_3**2
-lterm = phi_1**2 + phi_2**2 + phi_3**2
+mterm = r**2 + theta**2 + v**2 + omega**2
+lterm = r**2 + theta**2 + v**2 + omega**2
 
 mpc.set_objective(mterm=mterm, lterm=lterm)
 
 mpc.set_rterm(
-    phi_m_1_set=1e-2,
-    phi_m_2_set=1e-2
+    fr=1e-2,
+    fl=1e-2
 )
 
 #Constraints
 
-# Lower bounds on states:
-mpc.bounds['lower','_x', 'phi_1'] = -2*np.pi
-mpc.bounds['lower','_x', 'phi_2'] = -2*np.pi
-mpc.bounds['lower','_x', 'phi_3'] = -2*np.pi
-# Upper bounds on states
-mpc.bounds['upper','_x', 'phi_1'] = 2*np.pi
-mpc.bounds['upper','_x', 'phi_2'] = 2*np.pi
-mpc.bounds['upper','_x', 'phi_3'] = 2*np.pi
-
 # Lower bounds on inputs:
-mpc.bounds['lower','_u', 'phi_m_1_set'] = -2*np.pi
-mpc.bounds['lower','_u', 'phi_m_2_set'] = -2*np.pi
+mpc.bounds['lower','_u', 'fr'] = -5
+mpc.bounds['lower','_u', 'fl'] = -5
 # Lower bounds on inputs:
-mpc.bounds['upper','_u', 'phi_m_1_set'] = 2*np.pi
-mpc.bounds['upper','_u', 'phi_m_2_set'] = 2*np.pi
+mpc.bounds['upper','_u', 'fr'] = 5
+mpc.bounds['upper','_u', 'fl'] = 5
 
 #Scaling
-mpc.scaling['_x', 'phi_1'] = 2
-mpc.scaling['_x', 'phi_2'] = 2
-mpc.scaling['_x', 'phi_3'] = 2
+mpc.scaling['_x', 'r'] = 1
+mpc.scaling['_x', 'theta'] = 1
 
 #Uncertain Parameters
-inertia_mass_1 = 2.25*1e-4*np.array([1., 0.9, 1.1])
-inertia_mass_2 = 2.25*1e-4*np.array([1., 0.9, 1.1])
-inertia_mass_3 = 2.25*1e-4*np.array([1.])
+J_value = 3.234*np.array([1., 0.9, 1.1])
 
 mpc.set_uncertainty_values(
-    Theta_1 = inertia_mass_1,
-    Theta_2 = inertia_mass_2,
-    Theta_3 = inertia_mass_3
+    J = J_value,
 )
 
 mpc.setup()
@@ -126,9 +102,7 @@ simulator.set_param(t_step = 0.1)
 p_template = simulator.get_p_template()
 
 def p_fun(t_now):
-    p_template['Theta_1'] = 2.25e-4
-    p_template['Theta_2'] = 2.25e-4
-    p_template['Theta_3'] = 2.25e-4
+    p_template['J'] = 2.25e-4
     return p_template
 
 simulator.set_p_fun(p_fun)
@@ -136,7 +110,7 @@ simulator.set_p_fun(p_fun)
 simulator.setup()
 
 #Creating the control loop
-x0 = np.pi*np.array([1, 1, -1.5, 1, -1, 1, 0, 0]).reshape(-1,1)
+x0 = np.pi*np.array([1, 1, 1, 1]).reshape(-1,1)
 simulator.x0 = x0
 mpc.x0 = x0
 mpc.set_initial_guess()
@@ -158,13 +132,14 @@ fig.align_ylabels()
 
 for g in [sim_graphics, mpc_graphics]:
     # Plot the angle positions (phi_1, phi_2, phi_2) on the first axis:
-    g.add_line(var_type='_x', var_name='phi_1', axis=ax[0])
-    g.add_line(var_type='_x', var_name='phi_2', axis=ax[0])
-    g.add_line(var_type='_x', var_name='phi_3', axis=ax[0])
+    g.add_line(var_type='_x', var_name='r', axis=ax[0])
+    g.add_line(var_type='_x', var_name='theta', axis=ax[0])
+    g.add_line(var_type='_x', var_name='v', axis=ax[0])
+    g.add_line(var_type='_x', var_name='omega', axis=ax[0])
 
     # Plot the set motor positions (phi_m_1_set, phi_m_2_set) on the second axis:
-    g.add_line(var_type='_u', var_name='phi_m_1_set', axis=ax[1])
-    g.add_line(var_type='_u', var_name='phi_m_2_set', axis=ax[1])
+    g.add_line(var_type='_u', var_name='fr', axis=ax[1])
+    g.add_line(var_type='_u', var_name='fl', axis=ax[1])
 
 
 ax[0].set_ylabel('angle position [rad]')
@@ -173,7 +148,7 @@ ax[1].set_xlabel('time [s]')
 
 #Running simulator
 
-u0 = np.zeros((2,1))
+u0 = np.array([[10], [10]])
 for i in range(200):
     simulator.make_step(u0)
 
