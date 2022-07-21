c29c56af80278322904287ab555426fe1c3fa414
Hyago <hyago-brito@hotmail.com>
2022-07-21 18:00:10 -0300

    mpc_module update

--- boat_control.py
+++ boat_control.py
@@ -6,6 +6,7 @@ import math
 from zmqRemoteApi import RemoteAPIClient
 
 from mpc import MPC
+from mpc_module import MPCClass
 
 Ts = 0.05
 mass = 80
@@ -21,15 +22,11 @@ def quat2euler(h):
 
     return (roll, pitch, yaw)
 
-def get_error(X_d, X_m):
-    return X_d - X_m
-
 def extract_cartesian(velocity):
     linear = velocity[0][0]
     angular = velocity[-1][-1]
     x = linear*np.cos(angular)
     y = linear*np.sin(angular)
-
     return x, y
 
 def extract_error_information(error, X_m):
@@ -38,10 +35,11 @@ def extract_error_information(error, X_m):
     yaw = X_m[5][0]
     return v_x, v_y, yaw
 
-def extract_angular(v_x, v_y, yaw):
-    value = np.arctan2(v_y, v_x) - yaw
-    if value > np.pi:
-        value = value - 2*np.pi
+def extract_angular(v_x, v_y):
+    #value = np.arctan2(v_y, v_x) - yaw
+    value = np.arctan2(v_y, v_x)
+    #if value > np.pi:
+    #    value = value - 2*np.pi
     return value
 
 def extract_linear(v_x, v_y):
@@ -54,28 +52,51 @@ def calculate_angular(right_motor, left_motor, distance):
 def calculate_linear(right_motor, left_motor):
     return (right_motor+left_motor)/2
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 client = RemoteAPIClient()
 sim = client.getObject('sim')
+
+client.setStepping(True)
 
 sim.startSimulation()
 
 boat = sim.getObject('/Boat')
 boat_target = sim.getObject('/Boat_Target')
 
-position = sim.getObjectPosition(boat_target, boat)
-orientation = sim.getObjectQuaternion(boat_target, boat)
+#Target
+position = sim.getObjectPosition(boat_target, -1)
+orientation = sim.getObjectQuaternion(boat_target, -1)
 quat = np.array([orientation[3], orientation[0], orientation[1], orientation[2]]) # Remember: getObjectQuaternion has real part as last element
 (roll, pitch, target_yaw) = quat2euler(quat)
 
 X_d = np.array([[position[0]], [position[1]], [position[2]], [roll], [pitch], [target_yaw]])
 
+#Boat
 boat_position = sim.getObjectPosition(boat, -1)
 boat_orientation = sim.getObjectQuaternion(boat, -1)
 quat = np.array([boat_orientation[3], boat_orientation[0], boat_orientation[1], boat_orientation[2]]) # Remember: getObjectQuaternion has real part as last element
 (roll, pitch, yaw) = quat2euler(quat)
+boat_velocity = sim.getObjectVelocity(boat, -1)
 
 X_m = np.array([[boat_position[0]], [boat_position[1]], [boat_position[2]], [roll], [pitch], [yaw]])
 
+
+r = extract_linear(position[0], position[1])
+theta = extract_angular(position[0], position[1])
+v = 0
+omega = 0
+reference = np.array([position[0], position[1], theta, v, omega])
+
+r = extract_linear(boat_position[0], boat_position[1])
+theta = extract_angular(boat_position[0], boat_position[1])
+v = boat_velocity[0][0]
+omega = boat_velocity[-1][-1]
+actual_pos = np.array([boat_position[0], boat_position[1], yaw, v, omega])
+mpc = MPCClass(reference, Ts, actual_pos)
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #First data
 boat_data_x = []
 boat_data_y = []
@@ -83,33 +104,24 @@ target_position = [position[0], position[1]]
 initial_position = [0, 0]
 boat_data_x.append(boat_position[0])
 boat_data_y.append(boat_position[1])
-
-setpoint = np.ravel([[np.sqrt(position[0]**2 + position[1]**2), np.arctan2(position[1], position[0]), 0, 0] for i in range(0, N+1)])
-mpc = MPC(mass, np.array(initial_position), v_min, v_max, N, N_c, Ts)
-result = mpc.getNewForce(setpoint)
-force = result[1]
-print(force)
-
-Fr = 0
-Fl = 0
+Fr = [0]
+Fl = [0]
 
 while True:
     print('Final Fr: ' + str(Fr))
     print('Final Fl: ' + str(Fl))
-    sim.addForce(boat, [-0.3, -0.2, 0] , [Fr, 0, 0]) # Right Motor
-    sim.addForce(boat, [-0.3, 0.2, 0] , [Fl, 0, 0]) # Left Motor
-    #sim.addForceAndTorque(boat, [0, 0, 0] , [0, 0, angular])
+    sim.addForce(boat, [-0.3, -0.2, 0] , [Fr[0], 0, 0]) # Right Motor
+    sim.addForce(boat, [-0.3, 0.2, 0] , [Fl[0], 0, 0]) # Left Motor
 
-    time.sleep(Ts)
-
+    #time.sleep(Ts)
+    client.step()
     #Recollect information
-    #Target
 
-    position = sim.getObjectPosition(boat_target, boat)
-    orientation = sim.getObjectQuaternion(boat_target, boat)
+    #Target
+    position = sim.getObjectPosition(boat_target, -1)
+    orientation = sim.getObjectQuaternion(boat_target, -1)
     quat = np.array([orientation[3], orientation[0], orientation[1], orientation[2]]) # Remember: getObjectQuaternion has real part as last element
     (roll, pitch, yaw) = quat2euler(quat)
-
     X_d = np.array([[position[0]], [position[1]], [position[2]], [roll], [pitch], [yaw]])
 
     #Boat
@@ -119,20 +131,24 @@ while True:
     (roll, pitch, yaw) = quat2euler(quat)
     boat_velocity = sim.getObjectVelocity(boat, -1)
     v_x, v_y = extract_cartesian(boat_velocity)
-    v_linear = boat_velocity[0][0]
-    v_angular = boat_velocity[-1][-1]
-
+    v_linear = boat_velocity[0][0]      #v
+    v_angular = boat_velocity[-1][-1]   #w
 
     X_m = np.array([[boat_position[0]], [boat_position[1]], [boat_position[2]], [roll], [pitch], [yaw]])
+
+    #Data to plot
     boat_data_x.append(boat_position[0])
     boat_data_y.append(boat_position[1])
 
-    #Collect data
-    mpc.x_0 = np.array([0, 0, v_linear, v_angular])
-    setpoint = np.ravel([[np.sqrt(position[0]**2 + position[1]**2), np.arctan2(position[1], position[0]), 0, 0] for i in range(0, N+1)])
-    result = mpc.getNewForce(setpoint)
-    force = result[1]
-    print(force)
+    #MPC
+    r = extract_linear(boat_position[0], boat_position[1])
+    theta = extract_angular(boat_position[0], boat_position[1])
+    v = v_linear
+    omega = v_angular
+    actual_pos = np.array([boat_position[0], boat_position[1], yaw, v, omega])
+    print('Boat Position: ' + str(actual_pos))
+    print('Target Position: ' + str(reference))
+    force = mpc.run_mpc(actual_pos)
 
     Fr = force[0]
     Fl = force[1]
--- mpc_module.py
+++ mpc_module.py
@@ -2,191 +2,133 @@ import numpy as np
 import do_mpc
 from casadi import *
 
-#Creating Model
-model_type = 'continuous' # either 'discrete' or 'continuous'
-model = do_mpc.model.Model(model_type)
 
-'''
-states 	                _x 	Required
-inputs 	                _u 	Required
-algebraic 	        _z 	Optional
-parameter 	        _p 	Optional
-timevarying_parameter 	_tvp 	Optional
-'''
+class MPCClass():
+    def __init__(self, ref, ts, initial_position):
+        self.Ts = ts
+        self.reference = ref
 
-#Model Variables
-r = model.set_variable(var_type='_x', var_name='r', shape=(1,1))
-theta = model.set_variable(var_type='_x', var_name='theta', shape=(1,1))
-v = model.set_variable(var_type='_x', var_name='v', shape=(1,1))
-omega = model.set_variable(var_type='_x', var_name='omega', shape=(1,1))
+        #Creating Model
+        model_type = 'continuous' # either 'discrete' or 'continuous'
+        model = do_mpc.model.Model(model_type)
 
-# Two states for the desired (set) motor position:
-fr = model.set_variable(var_type='_u', var_name='fr')
-fl = model.set_variable(var_type='_u', var_name='fl')
+        '''
+        states 	                _x 	Required
+        inputs 	                _u 	Required
+        algebraic 	        _z 	Optional
+        parameter 	        _p 	Optional
+        timevarying_parameter 	_tvp 	Optional
+        '''
 
-#Model Parameters
-# As shown in the table above, we can use Long names or short names for the variable type.
-J = model.set_variable('parameter', 'J')
+        #Model Variables
+        #r = model.set_variable(var_type='_x', var_name='r', shape=(1,1))
+        x = model.set_variable(var_type='_x', var_name='x', shape=(1,1))
+        y = model.set_variable(var_type='_x', var_name='y', shape=(1,1))
+        theta = model.set_variable(var_type='_x', var_name='theta', shape=(1,1))
+        v = model.set_variable(var_type='_x', var_name='v', shape=(1,1))
+        omega = model.set_variable(var_type='_x', var_name='omega', shape=(1,1))
 
-#Right-hand-side equation
+        # Two states for the desired (set) motor position:
+        fr = model.set_variable(var_type='_u', var_name='fr')
+        fl = model.set_variable(var_type='_u', var_name='fl')
 
-d = 0.3
-l = 0.2
-m = 80
+        #Model Parameters
+        # As shown in the table above, we can use Long names or short names for the variable type.
+        #J = model.set_variable('parameter', 'J')
+        #J = 15
+        #Right-hand-side equation
 
-v_dot = d*omega**2 + (1/m)*(fr+fl)
-omega_dot = (l/(m*d**2 + J))*(fr-fl) - (m*d*v*omega)/(m*d**2 + J)
+        d = 0.3
+        l = 0.2
+        m = 80
 
+        J = 3.007 * m
 
-model.set_rhs('r', v)
-model.set_rhs('theta', omega)
-model.set_rhs('v', v_dot)
-model.set_rhs('omega', omega_dot)
-
-model.setup()
-
-#Configuring MPC controller
-mpc = do_mpc.controller.MPC(model)
-
-#Optimizer parameters
-setup_mpc = {
-    'n_horizon': 20,
-    't_step': 0.05,
-    'n_robust': 1,
-    'store_full_solution': True,
-}
-mpc.set_param(**setup_mpc)
-
-#Objective function
-mterm = r**2 + theta**2 + v**2 + omega**2
-lterm = r**2 + theta**2 + v**2 + omega**2
-
-mpc.set_objective(mterm=mterm, lterm=lterm)
-
-mpc.set_rterm(
-    fr=1e-2,
-    fl=1e-2
-)
-
-#Constraints
-
-# Lower bounds on inputs:
-mpc.bounds['lower','_u', 'fr'] = -5
-mpc.bounds['lower','_u', 'fl'] = -5
-# Lower bounds on inputs:
-mpc.bounds['upper','_u', 'fr'] = 5
-mpc.bounds['upper','_u', 'fl'] = 5
-
-#Scaling
-mpc.scaling['_x', 'r'] = 1
-mpc.scaling['_x', 'theta'] = 1
-
-#Uncertain Parameters
-J_value = 3.234*np.array([1., 0.9, 1.1])
-
-mpc.set_uncertainty_values(
-    J = J_value,
-)
-
-mpc.setup()
-
-#Configuring the simulator
-simulator = do_mpc.simulator.Simulator(model)
-
-#Simulator Parameters
-# Instead of supplying a dict with the splat operator (**), as with the optimizer.set_param(),
-# we can also use keywords (and call the method multiple times, if necessary):
-simulator.set_param(t_step = 0.1)
-
-#Uncertain Parameters
-p_template = simulator.get_p_template()
-
-def p_fun(t_now):
-    p_template['J'] = 2.25e-4
-    return p_template
-
-simulator.set_p_fun(p_fun)
-
-simulator.setup()
-
-#Creating the control loop
-x0 = np.pi*np.array([1, 1, 1, 1]).reshape(-1,1)
-simulator.x0 = x0
-mpc.x0 = x0
-mpc.set_initial_guess()
-
-#Graphic
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-# Customizing Matplotlib:
-mpl.rcParams['font.size'] = 18
-mpl.rcParams['lines.linewidth'] = 3
-mpl.rcParams['axes.grid'] = True
-
-mpc_graphics = do_mpc.graphics.Graphics(mpc.data)
-sim_graphics = do_mpc.graphics.Graphics(simulator.data)
-
-# We just want to create the plot and not show it right now. This "inline magic" supresses the output.
-fig, ax = plt.subplots(2, sharex=True, figsize=(16,9))
-fig.align_ylabels()
-
-for g in [sim_graphics, mpc_graphics]:
-    # Plot the angle positions (phi_1, phi_2, phi_2) on the first axis:
-    g.add_line(var_type='_x', var_name='r', axis=ax[0])
-    g.add_line(var_type='_x', var_name='theta', axis=ax[0])
-    g.add_line(var_type='_x', var_name='v', axis=ax[0])
-    g.add_line(var_type='_x', var_name='omega', axis=ax[0])
-
-    # Plot the set motor positions (phi_m_1_set, phi_m_2_set) on the second axis:
-    g.add_line(var_type='_u', var_name='fr', axis=ax[1])
-    g.add_line(var_type='_u', var_name='fl', axis=ax[1])
+        v_dot = (d*(omega**2)) + (1/m)*(fr+fl)
+        omega_dot = (l/(m*(d**2) + J))*(fr-fl) - (m*d*v*omega)/(m*(d**2) + J)
 
 
-ax[0].set_ylabel('angle position [rad]')
-ax[1].set_ylabel('motor angle [rad]')
-ax[1].set_xlabel('time [s]')
+        #model.set_rhs('r', v)
+        model.set_rhs('x', v*np.cos(theta))
+        model.set_rhs('y', v*np.sin(theta))
+        model.set_rhs('theta', omega)
+        model.set_rhs('v', v_dot)
+        model.set_rhs('omega', omega_dot)
 
-#Running simulator
+        model.setup()
 
-u0 = np.array([[10], [10]])
-for i in range(200):
-    simulator.make_step(u0)
+        #Configuring MPC controller
+        self.mpc = do_mpc.controller.MPC(model)
 
-sim_graphics.plot_results()
-# Reset the limits on all axes in graphic to show the data.
-sim_graphics.reset_axes()
-# Show the figure:
-fig.show()
+        #Optimizer parameters
+        setup_mpc = {
+            'n_horizon': 20,
+            't_step': self.Ts,
+            'n_robust': 0,
+            'store_full_solution': False,
+            'open_loop': True,
+            'nlpsol_opts': {
+                'ipopt.print_level': 0,
+                'ipopt.sb': 'yes',
+                'print_time': 0
+            }
+        }
+        self.mpc.set_param(**setup_mpc)
 
-#Running Optimizer
-u0 = mpc.make_step(x0)
-sim_graphics.clear()
-mpc_graphics.plot_predictions()
-mpc_graphics.reset_axes()
-# Show the figure:
-fig.show()
+        #Objective function
+        mterm = (self.reference[0] - x)**2 + \
+                (self.reference[1] - y)**2 + \
+                0*(self.reference[2] - theta)**2 + \
+                (self.reference[3] - v)**2 + \
+                (self.reference[4] - omega)**2
+        #mterm = r**2 + theta**2 + v**2 + omega**2
+        lterm = (self.reference[0] - x)**2 + \
+                (self.reference[1] - y)**2 + \
+                0*(self.reference[2] - theta)**2 + \
+                0*(self.reference[3] - v)**2 + \
+                0*(self.reference[4] - omega)**2
+        #lterm = r**2 + theta**2 + v**2 + omega**2
 
+        self.mpc.set_objective(mterm=mterm, lterm=lterm)
 
-#Running Control Loop
-simulator.reset_history()
-simulator.x0 = x0
-mpc.reset_history()
+        self.mpc.set_rterm(
+            fr=0,
+            fl=0
+        )
+        #[4.5240309  0.23942829 0.05027771 0.02493158]
+        #Constraints
 
-for i in range(20):
-    u0 = mpc.make_step(x0)
-    x0 = simulator.make_step(u0)
+        # Lower bounds on inputs:
+        self.mpc.bounds['lower','_u', 'fr'] = -20
+        self.mpc.bounds['lower','_u', 'fl'] = -20
+        # Lower bounds on inputs:
+        self.mpc.bounds['upper','_u', 'fr'] = 20
+        self.mpc.bounds['upper','_u', 'fl'] = 20
 
-# Plot predictions from t=0
-mpc_graphics.plot_predictions(t_ind=0)
-# Plot results until current time
-sim_graphics.plot_results()
-sim_graphics.reset_axes()
-fig.show()
+        #Scaling
+        self.mpc.scaling['_x', 'x'] = 1
+        self.mpc.scaling['_x', 'y'] = 1
+        self.mpc.scaling['_x', 'theta'] = 1
+        self.mpc.scaling['_x', 'v'] = 1
+        self.mpc.scaling['_x', 'omega'] = 1
 
+        #Uncertain Parameters
+        #J_value = 15*np.array([1., 0.9, 1.1])
 
-#Saving Data
-from do_mpc.data import save_results, load_results
-save_results([mpc, simulator])
+        #self.mpc.set_uncertainty_values(
+        #    J = J_value,
+        #)
+        self.mpc.x0 = initial_position
+        self.mpc.setup()
+        self.mpc.set_initial_guess()
 
+    def run_mpc(self, actual_position):
 
+        x0 = np.array(actual_position).reshape(-1,1)
+
+        #Creating the control loop
+        self.mpc.x0 = x0
+        #Running Optimizer
+        u0 = self.mpc.make_step(x0)
+        return u0
 
